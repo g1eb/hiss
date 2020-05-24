bbbe449d946dcebe6325482e0647e44d150f02fe
g1eb <mail@g1eb.com>
2020-05-24 11:52:56 -0700

    Keep track of how many mouse events have been seen and trigger when a certain threshold is passed

--- hiss.py
+++ hiss.py
@@ -8,6 +8,8 @@ COMBINATION = {
     keyboard.Key.ctrl,
 }
 currently_active = set()
+MOUSE_EVENTS = 0
+THRESHOLD = 50
 
 VOCAB = [
     'No!',
@@ -17,7 +19,7 @@ VOCAB = [
     'Bad Kitty!',
 ]
 
-def hiss(*args):
+def hiss():
     phrase = random.choice(VOCAB)
     os.system('say -v Daniel {}'.format(phrase))
 
@@ -38,10 +40,27 @@ keyboard_listener = keyboard.Listener(
 )
 keyboard_listener.start()
 
+def on_move(x, y):
+    global MOUSE_EVENTS
+    MOUSE_EVENTS += 1
+    if MOUSE_EVENTS >= THRESHOLD:
+        MOUSE_EVENTS = 0
+        hiss()
+
+def on_click(x, y, button, pressed):
+    hiss()
+
+def on_scroll(x, y, dx, dy):
+    global MOUSE_EVENTS
+    MOUSE_EVENTS += 1
+    if MOUSE_EVENTS >= THRESHOLD:
+        MOUSE_EVENTS = 0
+        hiss()
+
 mouse_listener = mouse.Listener(
-    on_move=hiss,
-    on_click=hiss,
-    on_scroll=hiss,
+    on_move=on_move,
+    on_click=on_click,
+    on_scroll=on_scroll,
 )
 mouse_listener.start()
 
