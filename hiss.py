d8786b8fe8b3d7e3f73336b3efef5a54f4eee371
g1eb <mail@g1eb.com>
2020-05-24 11:07:59 -0700

    Add support for ctrl/cmd + Esc combo used to exit the script

--- hiss.py
+++ hiss.py
@@ -2,14 +2,24 @@
 import os
 from pynput import keyboard
 
+COMBINATION = {
+    keyboard.Key.cmd,
+    keyboard.Key.ctrl,
+}
+currently_active = set()
+
 def hiss():
     os.system('say -v Daniel No!')
 
 def on_press(key):
     hiss()
+    if key in COMBINATION:
+        currently_active.add(key)
 
 def on_release(key):
-    if key == keyboard.Key.esc:
+    if key in currently_active:
+        currently_active.remove(key)
+    if key == keyboard.Key.esc and currently_active:
         return False
 
 def main():
